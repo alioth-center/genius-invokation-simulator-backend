b8b3b110fd2848c4a1a967d85c78c545f507745c
sunist-c <sunist@mail.sunist.cn>
2022-12-14 08:39:37 +0800

    revert main.go for drone build

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"github.com/sunist-c/genius-invokation-simulator-backend/protocal/http"
-	"time"
-)
-
 func main() {
-	http.StartHttpServer(8080)
-	time.Sleep(10 * time.Second)
+
 }

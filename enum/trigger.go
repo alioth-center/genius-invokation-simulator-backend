e2615b90ba8dfaa181c3f5ea473cd4736fac152b
sunist-c <sunist@mail.sunist.cn>
2022-12-20 17:30:32 +0800

    update triggers

--- enum/trigger.go
+++ enum/trigger.go
@@ -4,10 +4,12 @@ package enum
 type TriggerType byte
 
 const (
-	AfterAttack  TriggerType = iota // AfterAttack 攻击结算后触发
-	AfterDefence                    // AfterDefence 防御结算后触发
-	AfterEatFood                    // AfterEatFood 使用食物后触发
-	AfterHeal                       // AfterHeal 治疗后触发
-	AfterCharge                     // AfterCharge 充能后触发
-	AfterSwitch                     // AfterSwitch 切换角色后触发
+	AfterAttack     TriggerType = iota // AfterAttack 攻击结算后触发
+	AfterCharge                        // AfterCharge 充能后触发
+	AfterDefence                       // AfterDefence 防御结算后触发
+	AfterEatFood                       // AfterEatFood 使用食物后触发
+	AfterHeal                          // AfterHeal 治疗后触发
+	AfterRoundStart                    // AfterRoundStart 回合开始后触发
+	AfterRoundEnd                      // AfterRoundEnd 回合结束后触发
+	AfterSwitch                        // AfterSwitch 切换角色后触发
 )

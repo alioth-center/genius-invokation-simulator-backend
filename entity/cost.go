abcd1cf7ce4b9765a770ca4d2390bf43e1061567
sunist-c <sunist.c@outlook.com>
2023-01-03 11:27:20 +0800

    update entity.cost method and its unit-test

--- entity/cost.go
+++ entity/cost.go
@@ -7,6 +7,35 @@ type Cost struct {
 	total uint
 }
 
+// sub 从Cost中减少amount个element类型的元素骰子
+func (c *Cost) sub(element enum.ElementType, amount uint) {
+	if c.costs[element] >= amount {
+		c.costs[element] -= amount
+	} else {
+		c.costs[element] = 0
+	}
+}
+
+// add 向Cost中增加amount个element类型的元素骰子
+func (c *Cost) add(element enum.ElementType, amount uint) {
+	c.costs[element] += amount
+}
+
+// Pay 从Cost中减去other中的元素骰子，不含等价判断
+func (c *Cost) Pay(other Cost) {
+	for element, amount := range other.costs {
+		c.sub(element, amount)
+	}
+}
+
+// Add 想Cost中增加other中的所有元素骰子，不含等价判断
+func (c *Cost) Add(other Cost) {
+	for element, amount := range other.costs {
+		c.add(element, amount)
+	}
+}
+
+// Contains 判断Cost中是否包含other中的所有元素，含等价判断
 func (c Cost) Contains(other Cost) bool {
 	// 判断费用总数
 	if other.total > c.total {
@@ -37,17 +66,30 @@ func (c Cost) Contains(other Cost) bool {
 	return false
 }
 
+// Equals 判断Cost中的元素是否和other中的元素完全等价
 func (c Cost) Equals(other Cost) bool {
 	return c.Contains(other) && c.total == other.total
 }
 
-func newCostFromMap(m map[enum.ElementType]uint) *Cost {
+// NewCost 创建一个空Cost
+func NewCost() *Cost {
+	return &Cost{
+		costs: map[enum.ElementType]uint{},
+		total: 0,
+	}
+}
+
+// NewCostFromMap 从一个map创建Cost
+func NewCostFromMap(m map[enum.ElementType]uint) *Cost {
 	result := &Cost{
 		costs: map[enum.ElementType]uint{},
+		total: 0,
 	}
+
 	for elementType, amount := range m {
 		result.costs[elementType] = amount
 		result.total += amount
 	}
+
 	return result
 }

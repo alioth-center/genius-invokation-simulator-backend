7b3e55307eeaded01aa7c9ed7a6031ee6b21c138
sunist-c <sunist.c@outlook.com>
2023-01-05 09:59:36 +0800

    add burn card trigger

--- enum/trigger.go
+++ enum/trigger.go
@@ -5,6 +5,7 @@ type TriggerType byte
 
 const (
 	AfterAttack     TriggerType = iota // AfterAttack 攻击结算后触发
+	AfterBurnCard                      // AfterBurnCard 使用卡牌转换元素后触发
 	AfterCharge                        // AfterCharge 充能后触发
 	AfterDefence                       // AfterDefence 防御结算后触发
 	AfterEatFood                       // AfterEatFood 使用食物后触发

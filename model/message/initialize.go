219c25cc837333da55d487d583ffe2a4471144ae
sunist-c <sunist.c@outlook.com>
2023-01-06 16:57:37 +0800

    add initialize message

---
+++ model/message/initialize.go
@@ -0,0 +1,21 @@
+package message
+
+import "github.com/sunist-c/genius-invokation-simulator-backend/enum"
+
+type MatchingMessage struct {
+	UID        uint
+	Characters []uint
+	CardDeck   []uint
+}
+
+type GameOptions struct {
+	ReRollTime    uint
+	ElementAmount uint
+	StaticElement map[enum.ElementType]uint
+	RuleSet       uint
+}
+
+type InitializeMessage struct {
+	Players []MatchingMessage
+	Options GameOptions
+}

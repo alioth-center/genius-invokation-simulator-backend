657306999c6b9e8533a50d2eb2a301d70e105eab
sunist-c <sunist.c@outlook.com>
2023-01-06 14:48:27 +0800

    update main.go, add cli.run() and backend.run()

---
+++ exec/backend/main.go
@@ -0,0 +1,4 @@
+package backend
+
+func Run(port uint) {
+}
--- main.go
+++ main.go
@@ -1,153 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"github.com/sunist-c/genius-invokation-simulator-backend/entity"
-	"github.com/sunist-c/genius-invokation-simulator-backend/enum"
-	"github.com/sunist-c/genius-invokation-simulator-backend/model/context"
-	"github.com/sunist-c/genius-invokation-simulator-backend/model/modifier"
+	"flag"
+	"github.com/sunist-c/genius-invokation-simulator-backend/exec/backend"
+	"github.com/sunist-c/genius-invokation-simulator-backend/exec/cli"
+	"os"
 )
 
-type EmptyRule struct{}
-
-func (e EmptyRule) ReactionCalculate(types []enum.ElementType) (reaction enum.Reaction, elementRemains []enum.ElementType) {
-	return enum.ReactionNone, types
-}
-
-func (e EmptyRule) DamageCalculate(reaction enum.Reaction, targetCharacter uint, ctx *context.DamageContext) {
-}
-
-func (e EmptyRule) EffectCalculate(reaction enum.Reaction, targetPlayer entity.Player) (ctx *context.CallbackContext) {
-	return nil
-}
-
-func (e EmptyRule) Attach(originalElements []enum.ElementType, newElement enum.ElementType) (resultElements []enum.ElementType) {
-	return originalElements
-}
-
-func (e EmptyRule) Relative(reaction enum.Reaction, relativeElement enum.ElementType) bool {
-	return false
-}
-
-type Ganyu struct {
-	id uint
-}
-
-func (e Ganyu) ID() uint {
-	return e.id
-}
-
-func (e Ganyu) Affiliation() enum.Affiliation {
-	return enum.AffiliationLiyue
-}
-
-func (e Ganyu) Vision() enum.ElementType {
-	return enum.ElementCryo
-}
-
-func (e Ganyu) Weapon() enum.WeaponType {
-	return enum.WeaponBow
-}
-
-func (e Ganyu) MaxHP() uint {
-	return 16
-}
-
-func (e Ganyu) MaxMP() uint {
-	return 2
-}
-
-func (e Ganyu) Skills() map[uint]entity.Skill {
-	return map[uint]entity.Skill{1: Shuanghuashi{id: 1}}
-}
-
-type Shuanghuashi struct {
-	id uint
-}
-
-func (e Shuanghuashi) ID() uint {
-	return e.id
-}
-
-func (e Shuanghuashi) Type() enum.SkillType {
-	return enum.SkillNormalAttack
-}
-
-func (e Shuanghuashi) Cost() entity.Cost {
-	return *entity.NewCost()
-}
-
-func (e Shuanghuashi) BaseDamage(target, self uint, background []uint) *context.DamageContext {
-	return context.NewDamageContext(e.id, self, target, background, enum.ElementNone, 10)
-}
-
-type DefenceBuff struct {
-	id uint
-}
-
-func (d DefenceBuff) ID() uint {
-	return d.id
-}
-
-func (d DefenceBuff) Handler() func(ctx *modifier.Context[context.DamageContext]) {
-	return func(ctx *modifier.Context[context.DamageContext]) {
-		ctx.Data().SubActiveDamage(4)
+var (
+	args *argument = &argument{
+		mode: new(string),
+		port: new(uint),
+		conf: new(string),
 	}
+)
+
+type argument struct {
+	mode *string
+	conf *string
+	port *uint
 }
 
-func (d DefenceBuff) Clone() modifier.Modifier[context.DamageContext] {
-	return d
+func initArgs() {
+	flag.StringVar(args.conf, "conf", "", "setup the backend configuration file, highest priority")
+	flag.StringVar(args.mode, "mode", "backend", "setup the startup mode, available [backend, cli, ai]")
+	flag.UintVar(args.port, "port", 8086, "setup the http service port")
 }
 
-func (d DefenceBuff) RoundReset() {
-
+func init() {
+	initArgs()
 }
-
-func (d DefenceBuff) Effective() bool {
-	return true
-}
-
-func (d DefenceBuff) EffectLeft() uint {
-	return 0
-}
-
-type playerInfo struct {
-	uid        uint
-	name       string
-	cards      []entity.Card
-	characters map[uint]entity.Character
-}
-
-func (p playerInfo) UID() uint { return p.uid }
-
-func (p playerInfo) Name() string { return p.name }
-
-func (p playerInfo) Cards() []entity.Card { return p.cards }
-
-func (p playerInfo) Characters() map[uint]entity.Character { return p.characters }
 
 func main() {
-	ruleSet := entity.NewRuleSet(EmptyRule{})
-	ganyu1 := entity.NewCharacter(1, Ganyu{id: 2333}, ruleSet)
-	ganyu2 := entity.NewCharacter(2, Ganyu{id: 3333}, ruleSet)
-	player1 := entity.NewPlayer(playerInfo{
-		uid:        1,
-		name:       "player1",
-		cards:      []entity.Card{},
-		characters: map[uint]entity.Character{2333: ganyu1},
-	})
-	player2 := entity.NewPlayer(playerInfo{
-		uid:        2,
-		name:       "player2",
-		cards:      []entity.Card{},
-		characters: map[uint]entity.Character{3333: ganyu2},
-	})
-
-	core := entity.NewCore(ruleSet, []entity.Player{player1, player2})
-	fmt.Printf("player2.character: HP: %v Status: %v\n", ganyu2.HP(), ganyu2.Status())
-
-	core.ExecuteAttack(player1.UID(), player2.UID(), 1)
-	fmt.Printf("player2.character: HP: %v Status: %v\n", ganyu2.HP(), ganyu2.Status())
-	core.ExecuteAttack(player1.UID(), player2.UID(), 1)
-	fmt.Printf("player2.character: HP: %v Status: %v\n", ganyu2.HP(), ganyu2.Status())
+	flag.Parse()
+	switch *args.mode {
+	case "backend":
+		backend.Run(*args.port)
+	case "cli":
+		cli.Run()
+	case "ai":
+		panic("not implemented yet")
+	default:
+		os.Exit(0)
+	}
 }

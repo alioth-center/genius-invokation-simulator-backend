ec6b7808ff5f24a616c02a1e82db000bb242e818
sunist-c <sunist.c@outlook.com>
2023-01-06 16:01:52 +0800

    update player-status and add sender fields in action-message

--- enum/player.go
+++ enum/player.go
@@ -1,11 +1,13 @@
 package enum
 
+// PlayerStatus 玩家的状态
 type PlayerStatus byte
 
 const (
-	PlayerStatusReady PlayerStatus = iota
-	PlayerStatusWaiting
-	PlayerStatusActing
-	PlayerStatusDefeated
-	PlayerStatusViewing
+	PlayerStatusInitialized PlayerStatus = iota // PlayerStatusInitialized 玩家初始化完成
+	PlayerStatusReady                           // PlayerStatusReady 玩家在回合开始阶段准备
+	PlayerStatusWaiting                         // PlayerStatusWaiting 玩家在等待其他玩家操作
+	PlayerStatusActing                          // PlayerStatusActing 玩家正在执行操作
+	PlayerStatusDefeated                        // PlayerStatusDefeated 玩家被击败
+	PlayerStatusViewing                         // PlayerStatusViewing 玩家正在观战
 )

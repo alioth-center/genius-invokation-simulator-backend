6ebf924195a294eaceb477b779473a7703ce1f34
sunist-c <sunist@mail.sunist.cn>
2023-01-07 17:51:40 +0800

    update main/main.go, cli/main.go, backend/main.go with quit function

--- exec/cli/main.go
+++ exec/cli/main.go
@@ -1,4 +1,9 @@
 package cli
 
+import "time"
+
 func Run() {
+	time.Sleep(time.Second * 114514)
 }
+
+func Quit() {}
--- main.go
+++ main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/sunist-c/genius-invokation-simulator-backend/exec/backend"
 	"github.com/sunist-c/genius-invokation-simulator-backend/exec/cli"
+	"github.com/sunist-c/genius-invokation-simulator-backend/persistence"
 	"os"
+	"os/signal"
+	"syscall"
+)
+
+const (
+	aiMode      = "ai"
+	cliMode     = "cli"
+	backendMode = "backend"
 )
 
 var (
-	args *argument = &argument{
+	args = &argument{
 		mode: new(string),
 		port: new(uint),
 		conf: new(string),
 		save: new(bool),
 	}
+	sig = make(chan os.Signal, 4)
 )
 
 type argument struct {
@@ -30,18 +41,37 @@ func initArgs() {
 	flag.BoolVar(args.save, "save", true, "setup if to enable the persistence module")
 }
 
+func callQuit() {
+	s := <-sig
+	persistence.Quit()
+
+	switch *args.mode {
+	case backendMode:
+		backend.Quit()
+	case cliMode:
+		cli.Quit()
+	case aiMode:
+
+	}
+
+	fmt.Println("quit called:", s.String())
+	os.Exit(114)
+}
+
 func init() {
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGHUP)
 	initArgs()
+	flag.Parse()
 }
 
 func main() {
-	flag.Parse()
+	go callQuit()
 	switch *args.mode {
-	case "backend":
+	case backendMode:
 		backend.Run(*args.port)
-	case "cli":
+	case cliMode:
 		cli.Run()
-	case "ai":
+	case aiMode:
 		panic("not implemented yet")
 	default:
 		os.Exit(0)

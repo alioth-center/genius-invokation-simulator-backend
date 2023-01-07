a095e9e77c24a43d4536233f64ba621df94a02c0
sunist-c <sunist@mail.sunist.cn>
2023-01-07 17:46:44 +0800

    remove incorrect persistence.entity

--- persistence/character.go
+++
@@ -1,8 +0,0 @@
-package persistence
-
-import "github.com/sunist-c/genius-invokation-simulator-backend/entity"
-
-type CharacterInfo struct {
-	PersistenceIndex[entity.CharacterInfo]
-	Skills []uint
-}

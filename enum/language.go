fe2ac8ba28d1dfe6eb91b996acb97730b52e8dd1
sunist-c <sunist@mail.sunist.cn>
2023-01-08 17:05:09 +0800

    add i18n/language enums and structures

---
+++ enum/language.go
@@ -0,0 +1,15 @@
+package enum
+
+type Language uint
+
+const (
+	ChineseSimplified  Language = iota // ChineseSimplified 简体中文
+	ChineseTraditional                 // ChineseTraditional 繁體中文
+	English                            // English
+	French                             // French Français
+	German                             // German Deutsch
+	Japanese                           // Japanese 日本語
+	Korean                             // Korean 한국어
+	Russian                            // Russian Русский язык
+	Unknown                            // Unknown ያንሽቤ
+)
---
+++ model/localization/language.go
@@ -0,0 +1,51 @@
+package localization
+
+import "github.com/sunist-c/genius-invokation-simulator-backend/enum"
+
+type MultipleLanguagePack struct {
+	Languages          map[enum.Language]map[string]string
+	SupportedLanguages []enum.Language
+}
+
+// LanguagePack 语言包接口
+type LanguagePack interface {
+	Translate(original string, destLanguage enum.Language) (ok bool, result string)
+	Pack() MultipleLanguagePack
+}
+
+// languagePack 语言包
+type languagePack struct {
+	translations map[enum.Language]map[string]string
+	languages    []enum.Language
+}
+
+func (l languagePack) Translate(original string, destLanguage enum.Language) (ok bool, result string) {
+	if pack, hasLanguage := l.translations[destLanguage]; !hasLanguage {
+		return false, ""
+	} else {
+		result, ok = pack[original]
+		return ok, result
+	}
+}
+
+func (l languagePack) Pack() MultipleLanguagePack {
+	pack := MultipleLanguagePack{
+		Languages:          l.translations,
+		SupportedLanguages: l.languages,
+	}
+
+	return pack
+}
+
+func NewLanguagePack(translations map[enum.Language]map[string]string) LanguagePack {
+	pack := languagePack{
+		translations: translations,
+		languages:    []enum.Language{},
+	}
+
+	for language := range translations {
+		pack.languages = append(pack.languages, language)
+	}
+
+	return pack
+}

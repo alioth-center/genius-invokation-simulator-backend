920c0196a8919499eef75da2313740e27562c6aa
sunist-c <sunist.c@outlook.com>
2023-01-09 23:16:29 +0800

    update http configurations

--- protocol/http/config.go
+++ protocol/http/config.go
@@ -3,16 +3,22 @@ package http
 var (
 	config = &Config{
 		Middleware: MiddlewareConfig{
-			UUIDKey:                "uuid",
-			IPTranceKey:            "ip",
-			InterdictorTraceKey:    "interdicted",
-			InterdictorBlockedTime: 600,
+			UUIDKey:                 "uuid",
+			IPTranceKey:             "ip",
+			InterdictorTraceKey:     "interdicted",
+			InterdictorBlockedTime:  600,
+			InterdictorTriggerCount: 5,
+			QPSLimitTime:            1,
+		},
+		Backend: BackendConfig{
+			ListenPort: 8086,
 		},
 	}
 )
 
 type Config struct {
 	Middleware MiddlewareConfig `json:"middleware"`
+	Backend    BackendConfig    `json:"backend"`
 }
 
 func SetConfig(conf Config) {
@@ -30,4 +36,8 @@ type MiddlewareConfig struct {
 	InterdictorBlockedTime  uint   `json:"interdictor_blocked_time"`
 	InterdictorTriggerCount uint   `json:"interdictor_trigger_count"`
 	QPSLimitTime            uint   `json:"qps_limit_time"`
+}
+
+type BackendConfig struct {
+	ListenPort uint `json:"listen_port"`
 }

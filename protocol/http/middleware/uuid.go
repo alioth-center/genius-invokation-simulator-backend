7c56ffc38817f5c1de6ded2132c2ae99d6eb577f
sunist-c <sunist.c@outlook.com>
2023-01-09 18:16:16 +0800

    update uuid middleware

--- protocol/http/middleware/uuid.go
+++ protocol/http/middleware/uuid.go
@@ -5,12 +5,8 @@ import (
 	"github.com/sunist-c/genius-invokation-simulator-backend/util"
 )
 
-const (
-	uuidKey = "gisb/middleware.uuid"
-)
-
 // GetUUID 获取context中携带的uuid信息，若没有则生成并写入一个uuid
-func GetUUID(ctx *gin.Context) (uuid string) {
+func GetUUID(ctx *gin.Context, uuidKey string) (uuid string) {
 	if result, gotten := ctx.Get(uuidKey); !gotten {
 		uuid = util.GenerateUUID()
 		ctx.Set(uuidKey, uuid)
@@ -21,8 +17,8 @@ func GetUUID(ctx *gin.Context) (uuid string) {
 }
 
 // NewUUIDTagger 创建一个UUID标记器，将会往context中写入uuid
-func NewUUIDTagger() func(ctx *gin.Context) {
+func NewUUIDTagger(uuidKey string) func(ctx *gin.Context) {
 	return func(ctx *gin.Context) {
-		ctx.Set("uuid", util.GenerateUUID())
+		ctx.Set(uuidKey, util.GenerateUUID())
 	}
 }

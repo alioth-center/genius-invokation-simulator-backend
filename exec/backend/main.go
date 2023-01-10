07c2f774effd768fedbceb27aefed7214d27f031
sunist-c <sunist.c@outlook.com>
2023-01-10 12:00:30 +0800

    update backend mode serve logic

--- exec/backend/main.go
+++ exec/backend/main.go
@@ -1,9 +1,13 @@
 package backend
 
-import "github.com/sunist-c/genius-invokation-simulator-backend/protocol/http"
+import (
+	"github.com/sunist-c/genius-invokation-simulator-backend/protocol/http"
+	"github.com/sunist-c/genius-invokation-simulator-backend/protocol/http/service"
+)
 
 func Run(port uint) {
 	errChan := make(chan error)
+	service.InitServices(http.GetConfig().Middleware)
 	http.Serve(port, errChan)
 
 	err := <-errChan

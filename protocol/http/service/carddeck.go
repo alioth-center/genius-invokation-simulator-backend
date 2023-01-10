da22354d19d93bb6290eb332ff0aec94b07b5c5f
sunist-c <sunist.c@outlook.com>
2023-01-10 15:16:58 +0800

    add some card-deck services

--- protocol/http/service/carddeck.go
+++ protocol/http/service/carddeck.go
@@ -4,20 +4,62 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/sunist-c/genius-invokation-simulator-backend/protocol/http"
 	"github.com/sunist-c/genius-invokation-simulator-backend/protocol/http/middleware"
-	"time"
 )
 
 var (
-	cardDeckRouter *gin.RouterGroup
+	deckRouter *gin.RouterGroup
 )
 
 func initCardDeckService() {
-	cardDeckRouter = http.RegisterServices("/card_deck")
+	deckRouter = http.RegisterServices("/card_deck")
 
-	cardDeckRouter.Use(
+	deckRouter.Use(
 		append(
 			http.EngineMiddlewares,
-			middleware.NewQPSLimiter(time.Duration(cfg.QPSLimitTime)*time.Second, cfg.IPTranceKey),
+			middleware.NewQPSLimiter(cfg),
 		)...,
 	)
+
+	deckRouter.POST("",
+		middleware.NewQPSLimiter(cfg),
+	)
+}
+
+type UploadCardDeckRequest struct {
+	Owner           uint     `json:"owner"`
+	RequiredPackage []string `json:"required_package"`
+	Cards           []uint   `json:"cards"`
+	Characters      []uint   `json:"characters"`
+}
+
+type UploadCardDeckResponse struct {
+	ID              uint     `json:"id"`
+	Owner           uint     `json:"owner"`
+	RequiredPackage []string `json:"required_package"`
+	Cards           []uint   `json:"cards"`
+	Characters      []uint   `json:"characters"`
+}
+
+func uploadDeckServiceHandler() func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func deleteDeckServiceHandler() func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func updateDeckServiceHandler() func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+
+	}
+}
+
+func queryDeckServiceHandler() func(ctx *gin.Context) {
+	return func(ctx *gin.Context) {
+
+	}
 }

2f4a17fe5af2ca363e9c9cdb5bfef2bb06d84725
sunist-c <sunist.c@outlook.com>
2023-01-24 18:51:28 +0800

    fix benchmark errors in util, use random mac addr

--- util/benchmark_test.go
+++ util/benchmark_test.go
@@ -1,37 +1,26 @@
 package util
 
 import (
+	"math/rand"
 	"sync/atomic"
 	"testing"
 	"time"
 )
 
 func BenchmarkTestGenerateID(b *testing.B) {
-	if macs, err := GetMacAddresses(); err != nil || len(macs) == 0 {
-		b.Fatalf("get mac addresses failed: %v", err)
-	} else if mac, parsed := GetUintMacAddress(macs[0]); parsed != nil {
-		b.Fatalf("parse mac address failed: %v", parsed)
-	} else {
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			GenerateUID(mac, time.Now())
-		}
+	mac := rand.Uint64() % (1 << 48)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		GenerateUID(mac, time.Now())
 	}
+
 }
 
 func BenchmarkGenerateRealID(b *testing.B) {
-	var macAddr uint64
-	if macs, err := GetMacAddresses(); err != nil || len(macs) == 0 {
-		b.Fatalf("get mac addresses failed: %v", err)
-	} else if mac, parsed := GetUintMacAddress(macs[0]); parsed != nil {
-		b.Fatalf("parse mac address failed: %v", parsed)
-	} else {
-		macAddr = mac
-	}
-
+	mac := rand.Uint64() % (1 << 48)
 	uids := make([]uint64, 114514)
 	for i := 0; i < 114514; i++ {
-		uids[i] = GenerateUID(macAddr, time.Now())
+		uids[i] = GenerateUID(mac, time.Now())
 	}
 
 	var index int32 = 0

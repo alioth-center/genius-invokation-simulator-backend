d816de164ce534e65f87b7c3397fdaa1651848ae
sunist-c <sunist.c@outlook.com>
2023-02-23 17:11:24 +0800

    add languagePack definition in mod and its implement

---
+++ mod/definition/language.go
@@ -0,0 +1,18 @@
+package definition
+
+import "github.com/sunist-c/genius-invokation-simulator-backend/enum"
+
+type LanguagePack interface {
+	Language() enum.Language
+	ModDescription() ModDescription
+	GetCharacterDescription(longID uint64) (has bool, description CharacterDescription)
+	GetSkillDescription(longID uint64) (has bool, description SkillDescription)
+	GetEventDescription(longID uint64) (has bool, description EventDescription)
+	GetCardDescription(longID uint64) (has bool, description CardDescription)
+	GetModifierDescription(longID uint64) (has bool, description ModifierDescription)
+	AddCharacterDescription(description CharacterDescription)
+	AddSkillDescription(description SkillDescription)
+	AddEventDescription(description EventDescription)
+	AddCardDescription(description CardDescription)
+	AddModifierDescription(description ModifierDescription)
+}
